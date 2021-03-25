fe0970429f57756be3c514151671a67e14554eca
oanegros <oanegros@gmail.com>
2021-03-25 17:47:14 +0100

    Mutation and drawing starts

--- src/cells/mitochondrial_model/DNA.js
+++ src/cells/mitochondrial_model/DNA.js
@@ -3,15 +3,49 @@
 class DNA {
 
 		/* eslint-disable */ 
-	constructor (conf, parent) {
-        this.quality = new Array(this.conf["N_OXPHOS"]).fill(1);
+	constructor (conf, mt ,parent) {
+        this.oxphos_quality = new Array(this.conf["N_OXPHOS"]).fill(1);
+        this.translate_quality = new Array(this.conf["N_TRANSLATE"]).fill(1);
+        this.replicate_quality = new Array(this.conf["N_REPLICATE"]).fill(0);
+        this.mt = mt
+        this.conf = conf
+        this.replicateFlag = false
+        this.translateFlag = false
         if (parent instanceof DNA){
             this.quality = parent.quality
-            if 
+            if (this.mt.random > conf["MTDNA_MUT_RATE"] ){
+                this.mutate()
+            }
         }
-       
     }
 
+    mutate(){ 
+        indices = this.oxphos_quality.concat(this.translate_quality, this.replicate_quality).reduce(function(arr, e, i) {
+            if (e == 1) arr.push(i);
+            return arr;
+          }, [])
+        let ix = Math.floor(mt.random() * indices.length)
+
+        if (ix <= this.conf["N_OXPHOS"]){
+            this.oxphos_quality[ix] = 0
+        } else if (ix <= (this.conf["N_OXPHOS"] + this.conf["N_TRANSLATE"])){
+            this.translate_quality[ix - this.conf["N_OXPHOS"] ] = 0
+        } else {
+            this.replicate_quality[ix - this.conf["N_OXPHOS"] - this.conf["N_TRANSLATE"] ] = 0
+        }
+    }
+
+    notBusy(){
+        return (this.replicateFlag == false && this.translateFlag == false)
+    }
+
+    setReplicateFlag(bool){
+        this.replicateFlag = bool
+    }
+
+    setTranslateFlag(bool){
+        this.replicateFlag = bool
+    }
 }
 
 export default DNA
--- src/cells/mitochondrial_model/Mitochondrion.js
+++ src/cells/mitochondrial_model/Mitochondrion.js
@@ -1,7 +1,7 @@
 import Cell from "../Cell.js" 
 import DNA from "../DNA.js" 
 
-class HostCell extends Cell {
+class Mitochondrion extends Cell {
 
 		/* eslint-disable */ 
 	constructor (conf, kind, id, C, parent) {
@@ -9,7 +9,9 @@ class HostCell extends Cell {
         
         this.DNA = []
         this.oxphos_products = new Array(this.conf["N_OXPHOS"]).fill(0);
-        this.replication_products = new Array(this.conf["N_REP"]).fill(0);
+        this.translate_products = new Array(this.conf["N_TRANSLATE"]).fill(0);
+        this.replication_products = new Array(this.conf["N_REPLICATE"]).fill(0);
+       
         this.potential = 0
 
         this.individualParams = ["V", "P"]
@@ -20,8 +22,48 @@ class HostCell extends Cell {
 
 		if (parent instanceof Cell){ // copy on birth
             this.divideProducts(parent)
-		} 
+		} else {
+            for (let i = 0 ; i < this.conf["N_INIT_DNA"]; i++){
+                this.DNA[i] = new DNA(this.conf, this.mt)
+            }
+        }
 	}
+
+    update(){
+        if (this.DNA.length == 0 ){ return }
+
+        // takes bottleneck as rate
+        var rep_attempts = Math.min(this.replication_products) 
+        var translate_attempts = Math.min(this.oxpproducts)
+        var attempts = rep_attempts + translate_attempts
+
+        // replication and translation machinery try to find DNA to execute on
+        while (attempts > 0){
+            if (this.mt.random() < rep_attempts/attempts){
+                // attempt to find a DNA to replicate
+                let ix = Math.floor(mt.random() * this.DNA.length)
+                if (this.DNA[ix].notBusy()){ 
+                    this.DNA[ix].setReplicateFlag(true)
+                }
+                replicate_attempts--
+                attempts--
+            } else {
+                // attempt to translate
+                let ix = Math.floor(mt.random() * this.DNA.length)
+                if (this.DNA[ix].notBusy()){
+                    this.DNA[jx].setTranslateFlag(true)
+                }
+                translate_attempts-- //technically redundant
+                attempts--
+            }
+        }
+        for (var dna of this.DNA){
+            if (dna.translateFlag){
+
+            }
+        }
+
+    }
 
     divideProducts(parent){
         //do upon division
@@ -34,6 +76,9 @@ class HostCell extends Cell {
     fuse(partner) {
 
     }
+
+
+
 	/* eslint-disable */ 
 	getIndividualParam(param){
 		if (param == "V"){
