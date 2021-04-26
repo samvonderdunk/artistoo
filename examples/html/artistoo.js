c02b7f4594a63a70084d7715371e56d5ffcaeee8
oanegros <oanegros@gmail.com>
2021-04-26 17:04:33 +0200

    compile

--- examples/html/artistoo.js
+++ examples/html/artistoo.js
@@ -4824,10 +4824,6 @@ var CPM = (function (exports) {
 			super.birth(parent); // sets ParentId
 			this.host = parent.host;
 		}
-		
-		setHost(new_host){
-			this.host=new_host;
-		}
 	}
 
 	class DNA {
@@ -4848,9 +4844,7 @@ var CPM = (function (exports) {
 	            this.translate_quality = [...parent.translate_quality];
 	            this.replicate_quality = [...parent.replicate_quality];
 	            if (this.mt.random() < conf["MTDNA_MUT_RATE"] ){
-	                // console.log("before mutate", this.translate_quality)
 	                this.mutate();
-	                // console.log("after mutate", this.translate_quality)
 	            }
 	        }
 	    }
@@ -4876,153 +4870,68 @@ var CPM = (function (exports) {
 	    notBusy(){
 	        return (this.replicateFlag == false && this.translateFlag == false)
 	    }
-
-	    setReplicateFlag(bool){
-	        this.replicateFlag = bool;
-	    }
-
-	    setTranslateFlag(bool){
-	        this.translateFlag = bool;
-	    }
-
+	    
 	    sumQuality(){
 	        return  [this.oxphos_quality, this.translate_quality ,this.replicate_quality].reduce((t, e) => t.concat(e)).reduce((t, e) => t + e)
 	    }
 
 	}
 
-	class Mitochondrion extends Cell {
+	class Mitochondrion extends SubCell {
 
 		/* eslint-disable */ 
-		constructor (conf, kind, id, C, parent) {
-	        super(conf, kind, id, C, parent);
+	    constructor (conf, kind, id, mt) {
+			super(conf, kind, id, mt);
 	        
-	        this.DNA = [];
-	        this.oxphos_products = new Array(this.conf["N_OXPHOS"]).fill(this.conf["INIT_OXPHOS"]);
+			this.DNA = new Array(this.conf["N_INIT_DNA"]).fill(new DNA(this.conf, this.mt));
+	        this.oxphos_products = new Array(this.conf["N_OXPHOS"]).fill(80);
 	        this.translate_products = new Array(this.conf["N_TRANSLATE"]).fill(5);
 	        this.replication_products = new Array(this.conf["N_REPLICATE"]).fill(5);
 	       
 	        this.oxphos = this.conf["INIT_OXPHOS"];
 	        this.V = this.conf["INIT_OXPHOS"];
+		}
+		
+		clear(){
+			this.DNA = [];
+	        this.oxphos_products = new Array(this.conf["N_OXPHOS"]).fill(0);
+	        this.translate_products = new Array(this.conf["N_TRANSLATE"]).fill(0);
+	        this.replication_products = new Array(this.conf["N_REPLICATE"]).fill(0);
+		}
 
-	        this.individualParams = ["V", "P"];
-
-			// this.X = conf["INIT_X"][kind]
-			// this.Y = conf["INIT_Y"][kind]
-			// this.V = conf["INIT_V"][kind]
-
-			if (parent instanceof Cell){ // copy on birth
-	            this.divideProducts(parent);
-			} else {
-	            for (let i = 0 ; i < this.conf["N_INIT_DNA"]; i++){
-	                this.DNA[i] = new DNA(this.conf, this.mt);
-	            }
-	        }
+	    birth(parent, partition = 0.5){
+			this.clear();
+			this.divideProducts(parent.oxphos_products, this.oxphos_products, partition);
+	        this.divideProducts(parent.translate_products, this.translate_products, partition);
+	        this.divideProducts(parent.replication_products, this.replication_products, partition);
+		   
+			let new_parent = [];
+	        for (let dna of parent.DNA){
+				// if (!dna instanceof DNA){
+				// 	console.log("HYE")
+				// }
+	            if (this.mt.random() < partition){
+					this.DNA.push(dna);
+	            } else {
+	                new_parent.push(dna);
+	            }   
+			}
+			parent.DNA = new_parent;
+			
+			this.V = parent.V * partition;
+	        parent.V *= 1-partition;
 	    }
 
-	    // update(){
-	    //     this.oxphos += Math.min(this.oxphos_products) - this.conf["MITO_SHRINK"]
-
-	    //     if (this.DNA.length == 0 ){ return }
-	    //     // takes bottleneck as rate
-	    //     var rep_attempts = Math.min(this.replication_products) 
-	    //     var translate_attempts = Math.min(this.translate_products)
-	    //     var attempts = rep_attempts + translate_attempts
-
-	    //     // replication and translation machinery try to find DNA to execute on
-	    //     while (attempts > 0){
-	    //         if (this.mt.random() < rep_attempts/attempts){
-	    //             // attempt to find a DNA to replicate
-	    //             let ix = Math.floor(mt.random() * this.DNA.length)
-	    //             if (this.DNA[ix].notBusy()){ 
-	    //                 this.DNA[ix].setReplicateFlag(true)
-	    //             }
-	    //             replicate_attempts--
-	    //             attempts--
-	    //         } else {
-	    //             // attempt to translate
-	    //             let ix = Math.floor(mt.random() * this.DNA.length)
-	    //             if (this.DNA[ix].notBusy()){
-	    //                 this.DNA[jx].setTranslateFlag(true)
-	    //             }
-	    //             translate_attempts-- //technically redundant
-	    //             attempts--
-	    //         }
-	    //     }
-	    //     for (var dna of this.DNA){
-	    //         if (dna.translateFlag){
-	    //             console.log(dna.oxphos_quality, this.oxphos_products)
-	    //             this.oxphos_products += dna.oxphos_quality
-	    //             this.translate_products += dna.translate_quality
-	    //             this.replication_products += dna.replication_quality
-	    //             dna.setTranslateFlag(false)
-	    //         } else if (dna.replicateFlag) { 
-	    //             this.DNA.push(new DNA(conf, mt, dna))
-	    //             dna.setReplicateFlag(false)
-	    //         }
-	    //     }
-
-	    // }
-
-	    divideProducts(parent){
-	        let new_arr_1 = [[], [], []];
-	        let new_arr_2 = [[], [], []];
-	        for (const [which, arr] of [parent.oxphos_products, parent.translate_products, parent.replication_products].entries()){
-	            // console.log(arr, typeof arr)
-	            for (const [ix, product] of arr.entries()){
-	                new_arr_1[which].push(0);
-	                new_arr_2[which].push(0);
-	                for (let i = 0; i < product; i ++){
-	                    if (this.mt.random() > 0.5){
-	                        new_arr_1[which][ix]++;
-	                    } else {
-	                        new_arr_2[which][ix]++;
-	                    }   
-	                
-	                // let fluct = Math.floor(this.conf["NOISE"]* (2  *this.mt.random() - 1))
-	                // if ((product/2 - Math.abs(fluct)) < 0){ 
-	                //     fluct = Math.floor(product/2) * Math.sign(fluct)
-	                // }
-	                // // console.log(fluct)
-	                // new_arr_1[which].push(Math.floor(product/2) + fluct)
-	                // new_arr_2[which].push(Math.floor(product/2) - fluct)
-	                // if (product % 2 !== 0){
-	                    
-	                }  
+	    /* eslint-disable*/
+	    divideProducts(parent_arr, child_arr, partition){
+	        for (const [ix, product] of parent_arr.entries()){
+	            for (let i = 0; i < product; i ++){
+	                if (this.mt.random() < partition){
+	                    parent_arr[ix]--;
+	                    child_arr[ix]++;
+	                }
 	            }
-	        }
-	        // console.log("products:", [parent.oxphos_products, parent.translate_products, parent.replication_products],new_arr_1,new_arr_2)
-	        this.setProducts(new_arr_1[0], new_arr_1[1], new_arr_1[2]);
-	        parent.setProducts(new_arr_2[0], new_arr_2[1], new_arr_2[2]);
-	        
-
-	        // let fluct = this.conf["NOISE"]* (2  *this.mt.random() - 1)
-	        // if ((parent.DNA.length/2 - Math.abs(fluct)) < 0){
-	        //     fluct = parent.DNA.length/2  * Math.sign(fluct)
-	        // }
-	        // console.log('orig dna length', parent.DNA.length)
-	        let all_dna = this.shuffleArray(parent.DNA);
-	        parent.DNA = [];
-	        this.DNA = []; //redundant for birth call - but to be sure in later implementation
-	        for (let dna of all_dna){
-	            if (this.mt.random() > 0.5){
-	                this.DNA.push(dna);
-	            } else {
-	                parent.DNA.push(dna);
-	            }   
-	        }
-	        // for (const [ix, dna] of all_dna.entries()){
-	        //     if (ix < (Math.floor(parent.DNA.length/2) - Math.floor(fluct))){
-	        //         parent.DNA.push(dna)
-	        //     } else {
-	        //         this.DNA.push(dna)
-	        //     }
-	        // }
-	        // console.log('child 1 ', parent.DNA, "child 2", this.DNA)
-
-			this.V = parent.V/2;
-	        parent.V /= 2;
-	        // throw("HEY")
+	        }  
 	    }
 	    
 	    shuffleArray(unshuffled) {
@@ -5048,15 +4957,6 @@ var CPM = (function (exports) {
 
 	    }
 
-		/* eslint-disable */ 
-		getIndividualParam(param){
-			if (param == "V"){
-				// console.log(this.V)
-				return this.V
-			} 
-			throw("Implement changed way to get" + param + " constraint parameter per individual, or remove this from " + typeof this + " Cell class's indivualParams." )
-		}
-
 	    heteroplasmy(){
 	        // compute heteroplasmy
 	        if (this.DNA.length == 0){
@@ -5065,16 +4965,10 @@ var CPM = (function (exports) {
 	        let all_proteins = new DNA(this.conf, this.mt).sumQuality();
 	        let heteroplasmy = 0;
 	        for (let dna of this.DNA){
-	            if (dna.sumQuality() < all_proteins){
-	                heteroplasmy++;
-	            }
+	            heteroplasmy += (dna.sumQuality() < all_proteins);
 	        }
 	        return heteroplasmy/this.DNA.length
 	    }
-		// getColor(){
-		// 	return 100/this.Y
-		// }
-		
 	}
 
 	/**	This Stat creates a {@link CellArrayObject} with the border cellpixels of each cell on the grid. 
@@ -6114,6 +6008,9 @@ var CPM = (function (exports) {
 			// console.log( id )
 			// create a new ID for the second cell
 			let nid = C.makeNewCellID( C.cellKind( id ) );
+			if (C.hasOwnProperty("cells")){
+				C.birth(nid, id);
+			}
 
 			if (partition === 0.5){
 				for( let j = 0 ; j < cp.length ; j ++ ){
@@ -7606,7 +7503,7 @@ var CPM = (function (exports) {
 			for( let j = 0 ; j < N.length ; j ++ ){
 				tn = this.C.pixti( N[j] );
 				if( tn != tp ){ 
-					if (tn == 0 || tp == 0 || this.getParam("host", tn) != this.getParam("host", tp)){
+					if (tn == 0 || tp == 0 || this.cellParameter("host", tn) != this.cellParameter("host", tp)){
 						r += this.conf["J_EXT"][this.C.cellKind(tn)][this.C.cellKind(tp)];
 					} else {
 						// r -= this.conf["J"][this.C.cellKind(tn)][this.C.cellKind(tp)]
