72c9106cfd6f5bc446db16582deaeb0d0291cb7c
oanegros <oanegros@gmail.com>
2021-04-26 17:04:05 +0200

    update mito and host and refactor

--- src/cells/mitochondrial_model/HostCell.js
+++ src/cells/mitochondrial_model/HostCell.js
@@ -3,41 +3,16 @@ import SuperCell from "../SuperCell.js"
 class HostCell extends SuperCell {
 
 		/* eslint-disable */ 
-	constructor (conf, kind, id, C, parent) {
-        super(conf, kind, id, C, parent)
-        
+	constructor (conf, kind, id, mt) {
+		super(conf, kind, id, mt)
         this.mitochondria = []
         this.selfishness = 0.5
-        this.cell_products = 0
-
-        this.individualParams = ["V"]
-
-		// this.X = conf["INIT_X"][kind]
-		// this.Y = conf["INIT_Y"][kind]
-		// this.V = conf["INIT_V"][kind]
-
-		if (parent instanceof Cell){ // copy on birth
-            this.selfishness = parent.selfishness
-            this.divideProducts(parent)
-		} 
 	}
 
-    divideProducts(parent){
-        //do upon division
-    }
-	/* eslint-disable */ 
-	getIndividualParam(param){
-		if (param == "V"){
-			// console.log(this.V)
-			return this.V
-		} 
-		throw("Implement changed way to get" + param + " constraint parameter per individual, or remove this from " + typeof this + " Cell class's indivualParams." )
+	birth(parent){
+		super.birth(parent)
+		this.selfishness = parent.selfishness
 	}
-
-	// getColor(){
-	// 	return 100/this.Y
-	// }
-	
 }
 
 export default HostCell
