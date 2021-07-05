e3c193cf8ed0ae77555147824efe2f79b08c1a4e
oanegros <oanegros@gmail.com>
2021-07-05 15:16:52 +0200

    remove print

--- src/cells/mitochondrial_model/Mitochondrion.js
+++ src/cells/mitochondrial_model/Mitochondrion.js
@@ -131,7 +131,7 @@ class Mitochondrion extends SubCell {
        this.products.deprecate(this.conf['deprecation_rate'])
        this.bad_products.deprecate(this.conf['deprecation_rate'])
        
-       console.log(this.ros, this.oxphos)
+    //    console.log(this.ros, this.oxphos)
         for (const [ix, dna] of this.DNA.entries()){
             dna.mutate(this.conf['MTDNA_MUT_ROS'] * this.ros)
             if (this.C.random() < this.conf["dna_deprecation_rate"]){
