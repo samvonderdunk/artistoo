0e5c1a27434593a21cf8f82637c41489df5ce6fb
oanegros <oanegros@gmail.com>
2021-10-25 10:05:00 +0200

    Comment and update make_current
    also updates how string indexing is done in make_current, and calls local collect

--- hpc/make_current.sh
+++ hpc/make_current.sh
@@ -1,7 +1,19 @@
+# Uses preconfig.py and collect.py to convert a .js.tpl file (arg 2) and 
+# move all new config files as config.js to ordered subfolders in 
+# a new superfolder (arg 1)
+# Copy number (arg 3) is handed over to preconfig, to make multiple
+# copies of a parameter scan/seeded run (see preconfig.py help)
+
+# USAGE: 
+# ./make_current SUPERFOLDER_NAME CONFIGNAME.js.tpl COPY_NUMBER
+# example:
+#   ./make_current 211025_newsuperfolder config.js.tpl 5 
+
+
 mkdir $1
 cp $2 ./$1
 python3 preconfig.py $2 $3
 tplfile=$2
-# echo "${tplfile::-7}"
-python3 ~/collect.py run%%%%/config.js  "${tplfile::-7}"*.js > tmp
+
+python3 ./collect.py run%%%%/config.js  "${tplfile:  0:$((${#tplfile}-7))}"*.js > tmp
 mv run* $1/
